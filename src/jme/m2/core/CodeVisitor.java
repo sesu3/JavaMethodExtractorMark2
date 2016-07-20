def3d702be18626bbc89e1680c3c791699acc847
s_suzuki <unknown.worker.suzuki@gmail.com>
2016-07-20 21:20:36 +0900

    make project smaller

--- src/jme/m2/core/CodeVisitor.java
+++ src/jme/m2/core/CodeVisitor.java
@@ -1,24 +1,78 @@
 package jme.m2.core;
 
-import jme.m2.output.OutputIndicator;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import jme.m2.core.object.Variable;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 
 class CodeVisitor extends ASTVisitor
 {
-	
-	public CodeVisitor()
-	{
-		this.output=new OutputIndicator();
-	}
-	
+
 	public boolean visit(TypeDeclaration node)
 	{
-		this.output.printDeclarationState(node);
+		System.out.printf("[%s]%n",ASTTool.getKind(node));
+		System.out.printf("name:%s%n",ASTTool.getFullyQualifiedClassName(node));
+		System.out.print("modifier:");
+		for(Iterator<String> iter=ASTTool.getModifiers(node).iterator();iter.hasNext();){
+			String mod=iter.next();
+			if(iter.hasNext()){
+				System.out.print(mod+",");
+			}else{
+				System.out.print(mod);
+			}
+		}
+		System.out.printf("%nrange:%d,%d%n",ASTTool.getStartLineNumber(node),ASTTool.getEndLineNumber(node));
+		System.out.printf("super:%s%n",ASTTool.getSuperclassType(node));
+		System.out.print("field:");
+		for(Iterator<Variable> iter=ASTTool.getFields(node).iterator();iter.hasNext();){
+			Variable v=iter.next();
+			for(Iterator<String> iter2=ASTTool.getModifiers(v.getModifiers()).iterator();iter2.hasNext();){
+				System.out.print(iter2.next()+" ");
+			}
+			if(iter.hasNext()){
+				System.out.print(v.getType()+" "+v.getName()+",");
+			}else{
+				System.out.print(v.getType()+" "+v.getName());
+			}
+		}
+		System.out.println();
+		System.out.println();
+		for(Iterator<MethodDeclaration> iter=Arrays.asList(node.getMethods()).iterator();iter.hasNext();){
+			printDeclarationState(iter.next());
+		}
 		return super.visit(node);
 	}
-	
-	private OutputIndicator output;
+
+	private void printDeclarationState(MethodDeclaration node)
+	{
+		System.out.printf("[%s]%n",ASTTool.getKind(node));
+		System.out.printf("name:%s%n",ASTTool.getFullyQualifiedClassName(node));
+		System.out.printf("returnType:%s%n",ASTTool.getReturnType(node));
+		System.out.print("modifier:");
+		for(Iterator<String> iter=ASTTool.getModifiers(node).iterator();iter.hasNext();){
+			String mod=iter.next();
+			if(iter.hasNext()){
+				System.out.print(mod+",");
+			}else{
+				System.out.print(mod);
+			}
+		}
+		System.out.printf("%narguments:");
+		for(Iterator<SingleVariableDeclaration> iter=node.parameters().iterator();iter.hasNext();){
+			SingleVariableDeclaration argument=iter.next();
+			if(iter.hasNext()){
+				System.out.print(argument.getType()+" "+argument.getName()+",");
+			}else{
+				System.out.print(argument.getType()+" "+argument.getName());
+			}
+		}
+		System.out.printf("%nrange:%d,%d%n%n",ASTTool.getStartLineNumber(node),ASTTool.getEndLineNumber(node));
+	}
+
 }
--- src/jme/m2/main/Main.java
+++ src/jme/m2/main/Main.java
@@ -1,20 +1,36 @@
 package jme.m2.main;
 
+import java.io.IOException;
+
+import jme.m2.core.CodeParser;
+
 public class Main
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		Option option=new Option();
 		option.setOption(args);
 		String filePath=args[args.length-1];
 		System.out.println(filePath);
-		//ParseExecutable pe=new ParseExecutable();
-		//pe.setConfig(Arrays.asList(args));
-		//pe.execute();
-
+		if(!(isJavaFile(filePath)&&CodeParser.parsing(filePath,option))){
+			// error occured
+		}
 	}
-	
-	
-	
+
+	private static boolean isJavaFile(String fileName)
+	{
+		int position=fileName.lastIndexOf(".");
+		if(position==-1){
+			return false;
+		}else{
+			String extension=fileName.substring(position);
+			if(extension.equals(".java")){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+
 }
